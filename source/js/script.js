189087f48cc3e24f5fa30deea1fa880da4c4175a
nikolay tuvykin <tuvykin93@yandex.ru>
2023-10-11 08:57:15 +0300

    большой перезалив

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,14 @@
 /* в этот файл добавляет скрипты*/
+
+let navList = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.js-toggle-button');
+
+navToggle.addEventListener('click', function () {
+  if (navList.classList.contains('main-nav--closed')) {
+    navList.classList.remove('main-nav--closed');
+    navList.classList.add('main-nav--opened');
+  } else {
+    navList.classList.add('main-nav--closed');
+    navList.classList.remove('main-nav--opened');
+  }
+});
